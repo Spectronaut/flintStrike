1669e4953088d5005abc295d5a390e8b6620350f
Guido Diaz <spectronautmusic@gmail.com>
2022-12-19 19:39:01 -0700

    Test of ignore file for env

--- FlintStrike.sh
+++ FlintStrike.sh
@@ -6,4 +6,16 @@ output=$(python3 $pathToScript $1)
 echo "The output of this sript is $output"
 cd $output
 touch README.txt
-
+echo "Creating Environment..."
+sleep 5s
+python3 -m venv $1
+echo "Sucess..."
+sleep 5s
+source $1/bin/activate
+echo "$1 environment has been activated."
+say "$1 environment has been activated."
+sleep 5s
+echo "Initiating git... One monment please."
+say "Initiating git... One monment please."
+git init
+sleep 5s
